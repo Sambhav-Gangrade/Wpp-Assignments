68836a52b4f783b7e1d2a986813434317456d6c2
Sambhav-Gangrade <gangrade.sambhav@gmail.com>
2025-04-25 23:25:50 +0530

    Add files via upload

---
+++ q3.py
@@ -0,0 +1,21 @@
+def bigger_is_greater(w):
+    w = list(w)
+    n = len(w)
+    pivot = -1
+    for i in range(n - 2, -1, -1):
+        if w[i] < w[i + 1]:
+            pivot = i
+            break
+    for i in range(n - 1, pivot, -1):
+        if w[i] > w[pivot]:
+            break
+    w[pivot], w[i] = w[i], w[pivot]
+    w[pivot + 1:] = sorted(w[pivot + 1:])
+    return ''.join(w)
+
+
+t = int(input("Enter the number of test cases : "))
+for _ in range(t):
+    w = input().strip()
+    result = bigger_is_greater(w)
+    print(result)
